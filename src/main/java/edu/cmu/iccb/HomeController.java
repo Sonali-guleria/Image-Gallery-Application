5b8f7b53c85a12bf612987d66d36f070f940e00f
Sonali <sonali_guleria@outlook.com>
2016-10-05 22:28:46 -0400

    Update HomeController.java

--- src/main/java/edu/cmu/iccb/HomeController.java
+++ src/main/java/edu/cmu/iccb/HomeController.java
@@ -34,14 +34,14 @@ public class HomeController {
         return imageService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/images")
+    @RequestMapping(method = RequestMethod.GET, value = "/gallery")
     public String provideUploadInfo(Model model, RedirectAttributes redirectAttributes) {
       List<String> imageIds = imageService.getUploadedImages();        
       model.addAttribute("files", imageIds);     
         return "uploadForm";
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/images")
+    @RequestMapping(method = RequestMethod.POST, value = "/gallery")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
 
@@ -54,7 +54,7 @@ public class HomeController {
             redirectAttributes.addFlashAttribute("message", name + " failed to upload");
         }
 
-        return "redirect:/images";
+        return "redirect:/gallery";
     }
 
    public String homePage() {      
@@ -76,12 +76,12 @@ public String getLogin(RedirectAttributes redirectAttributes,
          
       SecurityContextHolder.getContext().setAuthentication(auth);
           
-      return "redirect:/images";
+      return "redirect:/gallery";
   }
   
 @RequestMapping(method = RequestMethod.POST, value = "/github/login")
 public String postLogin(RedirectAttributes redirectAttributes) {
-return "redirect:/images";
+return "redirect:/gallery";
 }
 
 
